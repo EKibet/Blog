9d53b89a974e983cd43342ec84fcfdb232e5ae21
EKibet <kibetedgar@gmail.com>
2018-09-15 21:10:56 +0300

    Adds ajax test

--- app/static/js/scripts.js
+++ app/static/js/scripts.js
@@ -1,13 +1,30 @@
-$(document).ready(function() {// Toggle Function
-  $('.toggle').click(function(){
-    // Switches the Icon
-    $(this).children('i').toggleClass('fa-pencil');
-    // Switches the forms  
-    $('.form').animate({
-      height: "toggle",
-      'padding-top': 'toggle',
-      'padding-bottom': 'toggle',
-      opacity: "toggle"
-    }, "slow");
-  });
+$(document).ready(function() {
+
+	$('form').on('submit', function(event) {
+
+		$.ajax({
+			data : {
+				name : $('#nameInput').val(),
+				email : $('#emailInput').val()
+			},
+			type : 'POST',
+			url : '/process'
+		})
+		.done(function(data) {
+
+			if (data.error) {
+				$('#errorAlert').text(data.error).show();
+				$('#successAlert').hide();
+			}
+			else {
+				$('#successAlert').text(data.name).show();
+				$('#errorAlert').hide();
+			}
+
+		});
+
+		event.preventDefault();
+
+	});
+
 });
