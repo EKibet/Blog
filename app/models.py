7e77c1556db0d5f29882909600d07065fb60d73a
EKibet <kibetedgar@gmail.com>
2018-09-16 14:26:02 +0300

    styling for navbar

--- app/models.py
+++ app/models.py
@@ -59,9 +59,27 @@ class Post(db.Model):
     post_content = db.Column(db.String(255))
     category = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime,index=True,default=datetime.utcnow)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    comments = db.relationship('Comments', backref='coment', lazy="dynamic")
+    user_id = db.Column(db.Integer, 
+                                    db.ForeignKey('users.id'))
+    comments = db.relationship('Comments', 
+                                    backref='coment', 
+                                    lazy="dynamic")
+    messages_sent = db.relationship('Message',
+                                    foreign_keys='Message.sender_id',
+                                    backref='author', lazy='dynamic')
+    messages_received = db.relationship('Message',
+                                        foreign_keys='Message.recipient_id',
+                                        backref='recipient', lazy='dynamic')
+    last_message_read_time = db.Column(db.DateTime)
 
+    # ...
+
+    def new_messages(self):
+        last_read_time = self.last_message_read_time or datetime(1900, 1, 1)
+        return Message.query.filter_by(recipient=self).filter(
+            Message.timestamp > last_read_time).count()
+
+            
     @classmethod
     def retrieve_posts(cls,id):
         posts = Post.filter_by(id=id).all()
@@ -87,3 +105,13 @@ class Comments(db.Model):
 
 	# def as_dict(self):
     # 		return {'details': self.details}    
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    sender_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    recipient_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Message {}>'.format(self.body)
