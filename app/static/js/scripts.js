89c632eb3dbe95c1ba5f8a83c757617910faf68a
EKibet <kibetedgar@gmail.com>
2018-09-16 10:06:35 +0300

    Updates ajax scripts

--- app/static/js/scripts.js
+++ app/static/js/scripts.js
@@ -1,30 +1,33 @@
-$(document).ready(function() {
-
-	$('form').on('submit', function(event) {
-
-		$.ajax({
-			data : {
-				name : $('#nameInput').val(),
-				email : $('#emailInput').val()
-			},
-			type : 'POST',
-			url : '/process'
-		})
-		.done(function(data) {
-
-			if (data.error) {
-				$('#errorAlert').text(data.error).show();
-				$('#successAlert').hide();
-			}
-			else {
-				$('#successAlert').text(data.name).show();
-				$('#errorAlert').hide();
-			}
-
-		});
-
-		event.preventDefault();
-
-	});
-
+$('document').ready(()=>{
+  $('#createform').submit((event)=>{
+    event.preventDefault()
+    pitch=$('#Pitch').val().trim()
+    category=$('#catchoose').val().trim()
+    if (pitch.length < 2){return}
+    $.ajax(
+      {
+        url:'/newpitch',
+        data:{
+          'pitch':pitch,
+          'category':category
+        },
+        method:'GET',
+        success:(data)=>{
+          $("#pitches").prepend(data)
+          $('#createform')[0].reset()
+        },
+        error: (data)=>{
+          alert('Could not post pitch')
+        }
+      }
+    )
+  });
+  submitcomment=(postid)=>{
+    $.post('/comments/'+postid, $('form#comment'+postid).serialize(),(data)=>{
+      $(data).hide().appendTo($('#comments'+postid)).show('fast');
+      count=$('#commentscount'+postid)
+      count.text(parseInt(count.text())+1)
+    });
+    [...$(".commentinput")].forEach(c=>c.value='')
+  }
 });
