b197da9044495f6317789fe02b5a33cd7e13f43f
EKibet <kibetedgar@gmail.com>
2018-09-17 13:42:44 +0300

    Deployment

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://yego:pass123@localhost/blog'
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://yego:pass123@localhost/blog'
     SECRET_KEY=os.environ.get('SECRET_KEY') or '1234'
     MAIL_SERVER ='smtp.googlemail.com'
     MAIL_PORT = 587
