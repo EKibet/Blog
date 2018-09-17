5855d23c57f148f56f531a1c8a994ad4152d53a0
EKibet <kibetedgar@gmail.com>
2018-09-17 13:01:33 +0300

    updates init

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,10 @@ from config import config_options,Config
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_mail import Mail
 
 db = SQLAlchemy()
-
+mail = Mail()
 boostrap =Bootstrap()
 login_manager = LoginManager()
 login_manager.login_view = 'auth.login'
@@ -21,7 +22,7 @@ def create_app(config_name):
 
     boostrap.init_app(app)
     login_manager.init_app(app)
-
+    mail.init_app(app)
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint, url_prefix='/authenticate')
     
